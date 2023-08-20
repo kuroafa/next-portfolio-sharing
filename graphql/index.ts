a3915d0b7786215335bb53c16386754f8f4cc099
wireflow <wireflowllc@gmail.com>
2023-08-19 22:22:47 -0400

    ......6

--- graphql/index.ts
+++ graphql/index.ts
@@ -56,7 +56,7 @@
   `;
   
   export const projectsQuery = `
-  query getProjects($categories: [String!], $endcursor: String) {
+  query getProjects($categories: [String!] , $endcursor: String) {
     projectSearch(first: 8, after: $endcursor, filter:  {category: {in: $categories}}) {
       pageInfo {
         hasNextPage
